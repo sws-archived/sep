01c6474766e41ff4a1911c42c79c8ff400a9839b
Megan Miller <meganem@stanford.edu>
2013-07-29 11:53:30 -0700

    Adding SEP JS file and updating links in templates.

---
+++ js/sep.js
@@ -0,0 +1,11 @@
+// Javascript for SEP
+
+jQuery(document).ready(function($) {
+	//Nav dropdown toggle
+	//$('.dropdown').addClass('toggle-hide');
+	
+	/*$('a[name="a"]').css('padding-top', 25 + $('#toc-nav').outerHeight(true));
+	$('a[name="b"]').css('padding-top', $('#toc-nav').outerHeight(true));
+    $('a[name="c"]').css('padding-top', $('#toc-nav').outerHeight(true));*/
+	
+});
