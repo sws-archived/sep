540dc27f2927e5fdfeac722329af78a9bac07048
Megan Miller <meganem@stanford.edu>
2013-07-29 16:40:33 -0700

    Testing main menu behavior for JS fallback

--- js/sep.js
+++ js/sep.js
@@ -3,6 +3,7 @@
 jQuery(document).ready(function($) {
 	// if Javascript is enabled, do these things:
 	$('body').removeClass('nojs');
+	$('.nav-collapse .dropdown').removeClass('open');
 	
 	/*$('a[name="a"]').css('padding-top', 25 + $('#toc-nav').outerHeight(true));
 	$('a[name="b"]').css('padding-top', $('#toc-nav').outerHeight(true));
