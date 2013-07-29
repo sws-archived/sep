49ed0cc21cc4302b4a420a24719634f3484669f3
Megan Miller <meganem@stanford.edu>
2013-07-29 16:53:29 -0700

    Additional JS fallback code for mirrors button and main menu

--- js/sep.js
+++ js/sep.js
@@ -4,8 +4,10 @@ jQuery(document).ready(function($) {
 	// if Javascript is enabled, do these things:
 	$('body').removeClass('nojs');
 	$('.nav-collapse .dropdown').removeClass('open');
+	$('.nav-collapse').removeClass('in');
 	$('#article-sidebar .btn-navbar').addClass('collapsed');
 	$('#article-sidebar #article-nav').removeClass('in');
+	$('#mirrors .btn-group').removeClass('open');
 	
 	/*$('a[name="a"]').css('padding-top', 25 + $('#toc-nav').outerHeight(true));
 	$('a[name="b"]').css('padding-top', $('#toc-nav').outerHeight(true));
