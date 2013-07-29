72871e395a49c5fe314036231dd7ffed99977b93
Megan Miller <meganem@stanford.edu>
2013-07-29 16:30:29 -0700

    Testing javascript fallback

--- js/sep.js
+++ js/sep.js
@@ -1,8 +1,8 @@
 // Javascript for SEP
 
 jQuery(document).ready(function($) {
-	//Nav dropdown toggle
-	//$('.dropdown').addClass('toggle-hide');
+	// if Javascript is enabled, do these things:
+	$('body').removeClass('nojs');
 	
 	/*$('a[name="a"]').css('padding-top', 25 + $('#toc-nav').outerHeight(true));
 	$('a[name="b"]').css('padding-top', $('#toc-nav').outerHeight(true));
