daf99e46e0546a1f4a5d8ff0751adbedcc616ed3
Brian Young <brianyoung@stanford.edu>
2013-07-30 13:39:36 -0700

    Reverted sticky sidebar changes

--- js/sep.js
+++ js/sep.js
@@ -9,27 +9,8 @@ $(document).ready(function() {
 	$('#article-sidebar #article-nav').removeClass('in');
 	$('#mirrors .btn-group').removeClass('open');
 	
-	/*$('a[name="a"]').css('padding-top', 25 + $('#toc-nav').outerHeight(true));
-	$('a[name="b"]').css('padding-top', $('#toc-nav').outerHeight(true));
-    $('a[name="c"]').css('padding-top', $('#toc-nav').outerHeight(true));*/
+	// $('a[name="a"]').css('padding-top', 25 + $('#toc-nav').outerHeight(true));
+	// $('a[name="b"]').css('padding-top', $('#toc-nav').outerHeight(true));
+    // $('a[name="c"]').css('padding-top', $('#toc-nav').outerHeight(true));
 	
-	// Dock each summary as it arrives just below the docked header, pushing the previous summary up the page.
-	var summaries = $('.sidebar');
-	summaries.each(function(i) {
-		var summary = $(summaries[i]);
-		var next = summaries[i + 1];
-
-		summary.scrollToFixed({
-			limit: function() {
-				var limit = 0;
-				if (next) {
-					limit = $(next).offset().top - $(this).outerHeight(true) - 10;
-				} else {
-					limit = $('.article-banner').offset().top - $(this).outerHeight(true) - 10;
-				}
-				return limit;
-			},
-			zIndex: 999
-		});
-	});
 });
