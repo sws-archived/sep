55efee46784836af48d38e9d61810746f9986f89
Brian Young <brianyoung@stanford.edu>
2013-07-29 17:22:25 -0700

    Fixed issue with sidebar navigation overlapping with footer section when scrolled to bottom of the page Fixed #7

--- js/sep.js
+++ js/sep.js
@@ -13,4 +13,23 @@ jQuery(document).ready(function($) {
 	$('a[name="b"]').css('padding-top', $('#toc-nav').outerHeight(true));
     $('a[name="c"]').css('padding-top', $('#toc-nav').outerHeight(true));*/
 	
+	// Dock each summary as it arrives just below the docked header, pushing the previous summary up the page.
+	var summaries = $('.sidebar');
+	summaries.each(function(i) {
+		var summary = $(summaries[i]);
+		var next = summaries[i + 1];
+
+		summary.scrollToFixed({
+			limit: function() {
+				var limit = 0;
+				if (next) {
+					limit = $(next).offset().top - $(this).outerHeight(true) - 10;
+				} else {
+					limit = $('.article-banner').offset().top - $(this).outerHeight(true) - 10;
+				}
+				return limit;
+			},
+			zIndex: 999
+		});
+	});
 });
